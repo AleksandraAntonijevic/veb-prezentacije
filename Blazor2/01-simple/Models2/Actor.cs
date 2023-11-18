586d1a04906dfe3759496fa8db247d237bff5e53
AleksandraAntonijevic <149418568+AleksandraAntonijevic@users.noreply.github.com>
2023-11-18 13:39:17 +0100

    new

--- Blazor2/01-simple/Models2/Actor.cs
+++ Blazor2/01-simple/Models2/Actor.cs
@@ -1,11 +1,14 @@
 namespace DSS.Models2
 {
-    public class Actor: Man
+    public class Actor: Man, IVanity
     {
 
         public int NumberOfOscars = 0;
         public int NumberOfBafars = 0;
         public double ImdbMark = 5;
+        public string Bragging(){
+            return "I, " + LastName + " " + FirstName + " am the best actor!!!";
+        }
 
     }
 }
